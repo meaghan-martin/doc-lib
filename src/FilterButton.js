42b252aba0b2285343e718daeedaf3e40c4e2998
Meaghan Martin <meaghanmartin@meaghans-air.mynetworksettings.com>
2023-02-12 19:58:07 -0500

    build

--- src/FilterButton.js
+++ src/FilterButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 export const FilterButton = ({tag, onClick}) => {
   return (
     <div className="filterlist">
-      <button className="filterButton" onClick={onClick}>{tag}</button>
+      <button onClick={onClick}>{tag}</button>
     </div>
   );
 }
