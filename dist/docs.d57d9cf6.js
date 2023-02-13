2620137e1f02d66f5ceae5963392204c74840899
Meaghan Martin <meaghanmartin@meaghans-air.mynetworksettings.com>
2023-02-12 19:47:03 -0500

    build

--- dist/docs.d57d9cf6.js
+++ dist/docs.d57d9cf6.js
@@ -9803,7 +9803,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63367" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64537" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
